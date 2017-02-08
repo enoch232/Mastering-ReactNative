07954feb5905d7757b1bac230d579cd22f33b649
Enoch Ko <enoch232@gmail.com>
2017-02-08 12:57:46 -0700

    attempt to build a react-native counter with redux

--- flexBox/src/components/Search.js
+++ flexBox/src/components/Search.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import {
   TextInput,
-  Text
+  Text,
+  View,
+  StyleSheet
 } from 'react-native'
 
+import * as globalstyles from '../styles/global'
 export default class Search extends Component {
   constructor(props){
     super(props)
@@ -13,8 +16,33 @@ export default class Search extends Component {
   }
   render(){
     return (
-      <TextInput>
-      </TextInput>
+      <View style={globalstyles.COMMON_STYLES.pageContainer}>
+         <View style={styles.search}>
+          <TextInput
+          style={styles.input}
+          onChangeText={text => this.setState({ searchBox: text })}
+          value = {this.state.searchBox}
+          />
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 35,
+    color: globalstyles.TEXT_COLOR,
+    paddingHorizontal: 5,
+    flex: 1
+  },
+  search: {
+    borderColor: globalstyles.MUTED_COLOR,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 5
+ }
+});
